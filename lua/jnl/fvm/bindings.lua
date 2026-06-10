b49cba154be3d96291d863b23cb5721c27c99c57
Jed Nelson <jed@nelson.ac>
2026-06-10 11:18:05 +0100

    Add type annotation to fvm bound operators

--- lua/jnl/fvm/bindings.lua
+++ lua/jnl/fvm/bindings.lua
@@ -1,6 +1,8 @@
 -- jnl/fvm/bindings.lua
-local I = require("jnl.fvm_internal")
-local M = {}
+local opt = require("jnl.core.optional")
+local I   = opt.require("jnl.fvm_internal")
+
+local M   = {}
 
 --
 -- Ctx construction
@@ -17,113 +19,269 @@ end
 -- Field operations
 --
 
-M.face_interp         = I.face_interp
-M.face_normal         = I.face_normal
-M.face_normal_c       = I.face_normal_c
-M.rhie_chow           = I.rhie_chow
-M.grad_gg             = I.grad_gg
-M.grad_lsq            = I.grad_lsq
-M.divergence_i        = I.divergence_i
-M.divergence_i_c      = I.divergence_i_c
-M.divergence_v        = I.divergence_v
-M.divergence_v_c      = I.divergence_v_c
-M.vorticity           = I.vorticity
-M.patch_gradient_flux = I.patch_gradient_flux
-M.ghost_copy          = I.ghost_copy
-M.ghost_k             = I.ghost_k
-M.diag_snapshot       = I.diag_snapshot
+---@type fun(mesh: Mesh2D, src: VecUD, dst: VecUD)
+M.face_interp           = I.face_interp
 
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, un: VecUD)
+M.face_normal           = I.face_normal
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, out: VecUD, pool: VecUD)
+M.face_normal_c         = I.face_normal_c
+
+---@type fun(mesh: Mesh2D, Ux: VecUD, Uy: VecUD, p: VecUD, gx: VecUD, gy: VecUD, ap_x: VecUD, ap_y: VecUD, out: VecUD)
+M.rhie_chow             = I.rhie_chow
+
+---@type fun(mesh: Mesh2D, face_phi: VecUD, gx: VecUD, gy: VecUD)
+M.grad_gg               = I.grad_gg
+
+---@type fun(mesh: Mesh2D, face_phi: VecUD, gx: VecUD, gy: VecUD)
+M.grad_lsq              = I.grad_lsq
+
+---@type fun(mesh: Mesh2D, un: VecUD, out: VecUD)
+M.divergence_i          = I.divergence_i
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, out: VecUD, pool: VecUD)
+M.divergence_i_c        = I.divergence_i_c
+
+---@type fun(mesh: Mesh2D, un: VecUD, out: VecUD)
+M.divergence_v          = I.divergence_v
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, out: VecUD, pool: VecUD)
+M.divergence_v_c        = I.divergence_v_c
+
+---@type fun(mesh: Mesh2D, phi: VecUD, gx: VecUD, gy: VecUD, out: VecUD)
+M.vorticity             = I.vorticity
+
+---@type fun(mesh: Mesh2D, phi: VecUD, gx: VecUD, gy: VecUD, gamma: number, patch: string): number
+M.patch_gradient_flux   = I.patch_gradient_flux
+
+---@type fun(mesh: Mesh2D, phi: VecUD)
+M.ghost_copy            = I.ghost_copy
+
+---@type fun(mesh: Mesh2D, phi: VecUD, k: number)
+M.ghost_k               = I.ghost_k
+
+---@type fun(mesh: Mesh2D, sys: FvSys, dst: VecUD)
+M.diag_snapshot         = I.diag_snapshot
 
 --
--- Operators
+-- Operators: unsteady
 --
 
-M.ddt_k               = I.ddt_k
-M.ddt_f               = I.ddt_f
-M.laplacian_k         = I.laplacian_k
-M.laplacian_f         = I.laplacian_f
-M.laplacian_nonorth_k = I.laplacian_nonorth_k
-M.laplacian_nonorth_f = I.laplacian_nonorth_f
-M.div_cds_k           = I.div_cds_k
-M.div_cds_f           = I.div_cds_f
-M.div_uds_k           = I.div_uds_k
-M.div_uds_f           = I.div_uds_f
-M.div_tvd_minmod      = I.div_tvd_minmod
-M.div_tvd_van_leer    = I.div_tvd_van_leer
-M.div_tvd_superbee    = I.div_tvd_superbee
-M.su_v_k              = I.su_v_k
-M.su_v_f              = I.su_v_f
-M.su_v_fs             = I.su_v_fs
-M.su_i_k              = I.su_i_k
-M.su_i_f              = I.su_i_f
-M.su_i_fs             = I.su_i_fs
-M.sp_v_k              = I.sp_v_k
-M.sp_v_f              = I.sp_v_f
-M.sp_v_fs             = I.sp_v_fs
-M.sp_i_k              = I.sp_i_k
-M.sp_i_f              = I.sp_i_f
-M.sp_i_fs             = I.sp_i_fs
+---@type fun(sys: FvSys, mesh: Mesh2D, rho: number, dt: number, phi_old: VecUD)
+M.ddt_k                 = I.ddt_k
 
+---@type fun(sys: FvSys, mesh: Mesh2D, rho: VecUD, dt: number, phi_old: VecUD)
+M.ddt_f                 = I.ddt_f
+
+--
+-- Operators: laplacian/diffusion
+--
+
+---@type fun(sys: FvSys, mesh: Mesh2D, gamma: number)
+M.laplacian_k           = I.laplacian_k
+
+---@type fun(sys: FvSys, mesh: Mesh2D, gamma: VecUD)
+M.laplacian_f           = I.laplacian_f
+
+---@type fun(sys: FvSys, mesh: Mesh2D, gamma: number, gx: VecUD, gy: VecUD)
+M.laplacian_nonorth_k   = I.laplacian_nonorth_k
+
+---@type fun(sys: FvSys, mesh: Mesh2D, gamma: VecUD, gx: VecUD, gy: VecUD)
+M.laplacian_nonorth_f   = I.laplacian_nonorth_f
+
+--
+-- Operators: divergence/convection
+-- _k = constant density, _f = field density.
+-- tvd uses uds as the implicit base; div_dc adds the explicit correction.
+--
+
+---@type fun(sys: FvSys, mesh: Mesh2D, rho: number, un: VecUD)
+M.div_uds_k             = I.div_uds_k
+
+---@type fun(sys: FvSys, mesh: Mesh2D, rho: VecUD, un: VecUD)
+M.div_uds_f             = I.div_uds_f
+
+---@type fun(sys: FvSys, mesh: Mesh2D, rho: number, un: VecUD)
+M.div_cds_k             = I.div_cds_k
+
+---@type fun(sys: FvSys, mesh: Mesh2D, rho: VecUD, un: VecUD)
+M.div_cds_f             = I.div_cds_f
+
+---@type fun(sys: FvSys, mesh: Mesh2D, phi: VecUD, gx: VecUD, gy: VecUD, un: VecUD)
+M.div_tvd_minmod        = I.div_tvd_minmod
+
+---@type fun(sys: FvSys, mesh: Mesh2D, phi: VecUD, gx: VecUD, gy: VecUD, un: VecUD)
+M.div_tvd_van_leer      = I.div_tvd_van_leer
+
+---@type fun(sys: FvSys, mesh: Mesh2D, phi: VecUD, gx: VecUD, gy: VecUD, un: VecUD)
+M.div_tvd_superbee      = I.div_tvd_superbee
+
+--
+-- Operators: source terms
+-- _v = volumetric (value is per unit volume; multiplied by cell vol internally)
+-- _i = integrated (value is total per cell; no volume weighting)
+-- _k = constant scalar, _f = field vec, _fs = scaled field vec
+--
+
+---@type fun(sys: FvSys, mesh: Mesh2D, k: number)
+M.su_v_k                = I.su_v_k
+
+---@type fun(sys: FvSys, mesh: Mesh2D, f: VecUD)
+M.su_v_f                = I.su_v_f
+
+---@type fun(sys: FvSys, mesh: Mesh2D, scale: number, f: VecUD)
+M.su_v_fs               = I.su_v_fs
+
+---@type fun(sys: FvSys, mesh: Mesh2D, k: number)
+M.su_i_k                = I.su_i_k
+
+---@type fun(sys: FvSys, mesh: Mesh2D, f: VecUD)
+M.su_i_f                = I.su_i_f
+
+---@type fun(sys: FvSys, mesh: Mesh2D, scale: number, f: VecUD)
+M.su_i_fs               = I.su_i_fs
+
+---@type fun(sys: FvSys, mesh: Mesh2D, k: number)
+M.sp_v_k                = I.sp_v_k
+
+---@type fun(sys: FvSys, mesh: Mesh2D, f: VecUD)
+M.sp_v_f                = I.sp_v_f
+
+---@type fun(sys: FvSys, mesh: Mesh2D, scale: number, f: VecUD)
+M.sp_v_fs               = I.sp_v_fs
+
+---@type fun(sys: FvSys, mesh: Mesh2D, k: number)
+M.sp_i_k                = I.sp_i_k
+
+---@type fun(sys: FvSys, mesh: Mesh2D, f: VecUD)
+M.sp_i_f                = I.sp_i_f
+
+---@type fun(sys: FvSys, mesh: Mesh2D, scale: number, f: VecUD)
+M.sp_i_fs               = I.sp_i_fs
 
 --
 -- Boundary conditions
 --
 
-M.BC_NEUMANN      = I.BC_NEUMANN
-M.BC_DIRICHLET    = I.BC_DIRICHLET
-M.BC_ROBIN        = I.BC_ROBIN
+---@type integer  JNL_BC_NEUMANN = 0
+M.BC_NEUMANN            = I.BC_NEUMANN
 
--- scalar patch
-M.patch_s_fill_d  = I.patch_s_fill_d
-M.patch_s_fill_n  = I.patch_s_fill_n
-M.patch_s_fill_r  = I.patch_s_fill_r
-M.patch_s_close_d = I.patch_s_close_d
-M.patch_s_close_n = I.patch_s_close_n
-M.patch_s_close_r = I.patch_s_close_r
+---@type integer  JNL_BC_DIRICHLET = 1
+M.BC_DIRICHLET          = I.BC_DIRICHLET
 
--- vector patch
-M.patch_v_fill_d  = I.patch_v_fill_d
-M.patch_v_fill_n  = I.patch_v_fill_n
-M.patch_v_fill_nt = I.patch_v_fill_nt
+---@type integer  JNL_BC_ROBIN = 2
+M.BC_ROBIN              = I.BC_ROBIN
 
+-- scalar patch ghost fill: prime the ghost layer before sys_reset
 
--- scalar baffle-region
+---@type fun(mesh: Mesh2D, phi: VecUD, patch: string, value: number)
+M.patch_s_fill_d        = I.patch_s_fill_d
+
+---@type fun(mesh: Mesh2D, phi: VecUD, patch: string, grad_n: number)
+M.patch_s_fill_n        = I.patch_s_fill_n
+
+---@type fun(mesh: Mesh2D, phi: VecUD, patch: string, a: number, b: number, c: number)
+M.patch_s_fill_r        = I.patch_s_fill_r
+
+-- scalar patch implicit close: enforce BCs in the linear system
+
+---@type fun(sys: FvSys, mesh: Mesh2D, patch: string, value: number)
+M.patch_s_close_d       = I.patch_s_close_d
+
+---@type fun(sys: FvSys, mesh: Mesh2D, patch: string, grad_n: number)
+M.patch_s_close_n       = I.patch_s_close_n
+
+---@type fun(sys: FvSys, mesh: Mesh2D, patch: string, a: number, b: number, c: number)
+M.patch_s_close_r       = I.patch_s_close_r
+
+-- vector patch ghost fill
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, patch: string, ux_val: number, uy_val: number)
+M.patch_v_fill_d        = I.patch_v_fill_d
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, patch: string, ux_gn: number, uy_gn: number)
+M.patch_v_fill_n        = I.patch_v_fill_n
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, patch: string, nkind: integer, nval: number, tkind: integer, tval: number)
+M.patch_v_fill_nt       = I.patch_v_fill_nt
+
+-- scalar baffle-region ghost fill
+
+---@type fun(mesh: Mesh2D, phi: VecUD, baffle: string, region: integer, value: number)
 M.bregion_s_fill_d      = I.bregion_s_fill_d
+
+---@type fun(mesh: Mesh2D, phi: VecUD, baffle: string, region: integer, grad_n: number)
 M.bregion_s_fill_n      = I.bregion_s_fill_n
+
+---@type fun(mesh: Mesh2D, phi: VecUD, baffle: string, region: integer, a: number, b: number, c: number)
 M.bregion_s_fill_r      = I.bregion_s_fill_r
+
+-- scalar baffle-region implicit close
+
+---@type fun(sys: FvSys, mesh: Mesh2D, baffle: string, region: integer, value: number)
 M.bregion_s_close_d     = I.bregion_s_close_d
+
+---@type fun(sys: FvSys, mesh: Mesh2D, baffle: string, region: integer, grad_n: number)
 M.bregion_s_close_n     = I.bregion_s_close_n
+
+---@type fun(sys: FvSys, mesh: Mesh2D, baffle: string, region: integer, a: number, b: number, c: number)
 M.bregion_s_close_r     = I.bregion_s_close_r
 
--- vector baffle-region
+-- vector baffle-region ghost fill
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, baffle: string, region: integer, ux_val: number, uy_val: number)
 M.bregion_v_fill_d      = I.bregion_v_fill_d
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, baffle: string, region: integer, ux_gn: number, uy_gn: number)
 M.bregion_v_fill_n      = I.bregion_v_fill_n
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, baffle: string, region: integer, nkind: integer, nval: number, tkind: integer, tval: number)
 M.bregion_v_fill_nt     = I.bregion_v_fill_nt
 
--- whole-baffle scalar
+-- whole-baffle scalar helpers
+
+---@type fun(mesh: Mesh2D, phi: VecUD, baffle: string)
 M.baffle_s_fill_insul   = I.baffle_s_fill_insul
+
+---@type fun(sys: FvSys, mesh: Mesh2D, baffle: string)
 M.baffle_s_close_insul  = I.baffle_s_close_insul
+
+---@type fun(mesh: Mesh2D, phi: VecUD, baffle: string)
 M.baffle_s_fill_cont    = I.baffle_s_fill_cont
+
+---@type fun(sys: FvSys, mesh: Mesh2D, baffle: string)
 M.baffle_s_close_cont   = I.baffle_s_close_cont
+
+---@type fun(sys: FvSys, mesh: Mesh2D, baffle: string, conductance: number)
 M.baffle_s_close_cc     = I.baffle_s_close_cc
+
+---@type fun(sys: FvSys, mesh: Mesh2D, baffle: string, resistance: number)
 M.baffle_s_close_cr     = I.baffle_s_close_cr
 
--- all-baffles scalar
+-- all-baffles scalar helpers
+
+---@type fun(mesh: Mesh2D, phi: VecUD)
 M.baffles_s_fill_insul  = I.baffles_s_fill_insul
+
+---@type fun(sys: FvSys, mesh: Mesh2D)
 M.baffles_s_close_insul = I.baffles_s_close_insul
 
--- whole-baffle vector
+-- whole-baffle vector helpers
+
+---@type fun(mesh: Mesh2D, ux: VecUD, uy: VecUD, baffle: string)
 M.baffle_v_fill_cont    = I.baffle_v_fill_cont
 
 -- debug
-M.bc_assert_all_closed  = I.bc_assert_all_closed
 
+---@type fun(sys: FvSys)
+M.bc_assert_all_closed  = I.bc_assert_all_closed
 
 --
 -- Solver dispatch
 --
 
-local solver_ctors = {
+local solver_ctors      = {
 	cg_jac        = function(sys, phi, tol, _) return sys:cg_jac(phi, tol) end,
 	cg_dic        = function(sys, phi, tol, _) return sys:cg_dic(phi, tol) end,
 	bicgstab_jac  = function(sys, phi, tol, _) return sys:bicgstab_jac(phi, tol) end,
@@ -131,10 +289,22 @@ local solver_ctors = {
 	gmres_dilu    = function(sys, phi, tol, opts) return sys:gmres_dilu(phi, tol, opts.restart or 20) end,
 }
 
+---@param name string
+---@return boolean
 function M.valid_solver(name)
 	return solver_ctors[name:lower()] ~= nil
 end
 
+---@class SolverOpts
+---@field solver    string?   default "bicgstab_dilu"
+---@field tol       number?   default 1e-6
+---@field max_iters integer?  default 1000
+---@field restart   integer?  GMRES restart dimension, default 20
+
+---@param sys  FvSys
+---@param phi  VecUD
+---@param opts SolverOpts?
+---@return BicgstabDiluSolve | BicgstabJacSolve | CgJacSolve | CgDicSolve | GmresDiluSolve
 function M.make_solver(sys, phi, opts)
 	opts       = opts or {}
 	local name = (opts.solver or "bicgstab_dilu"):lower()
@@ -144,11 +314,24 @@ function M.make_solver(sys, phi, opts)
 	return ctor(sys, phi, tol, opts)
 end
 
+---@param sys   FvSys
+---@param phi   VecUD
+---@param omega number?  default 1.0
+---@return JacobiSmoother
 function M.make_smoother(sys, phi, omega)
 	return sys:jacobi_smoother(phi, omega or 1.0)
 end
 
--- Blocking solve with no yield points. Used outside case:step() contexts.
+---@class SolveResult
+---@field change    number
+---@field n_iters   integer
+---@field residual  number
+---@field breakdown boolean
+
+---@param sys  FvSys
+---@param phi  VecUD
+---@param opts SolverOpts?
+---@return SolveResult
 function M.solve_once(sys, phi, opts)
 	opts        = opts or {}
 	local max_k = opts.max_iters or 1000
